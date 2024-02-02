a5ee06aa3f42296f7d9da5cfc60a9d821e5a371c
rayala36 <rayala36@miners.utep.edu>
2024-02-01 21:01:23 -0700

    Explanations Updated

--- wordCount.py
+++ wordCount.py
@@ -13,12 +13,16 @@ if len(sys.argv) != 3:
     
 def read():
 
+    # Input file is the first parameter
     inFile = sys.argv[1]
-    
+
+    # All words will be initially stored in an array; "with" esnures opening and closing of input file
     words = []
     with open(inFile, 'r') as inputFile:
+        # For each line in the file, all punctuation marks are replaced with a space
         for line in inputFile:
             line = re.sub(r'[.," !;:?/-]', ' ', line)
+            # Words are placed  in a list, using space or tab as the regex
             newWords = re.split('[ \t]', line)
             words.extend(word.lower() for word in newWords)
     return words
@@ -29,6 +33,7 @@ def write():
     outputFile = open("output.txt", "w")
     alphabetizedList = sorted(words)
     for word in alphabetizedList:
+        # If a word is a alhabetic and not already in the hash table, the word and its count are added and written to output.txt.
         if word.isalpha() and word not in wordsMappedToCounts:
             wordsMappedToCounts[word] = alphabetizedList.count(word)
             outputFile.write(f"{word} {wordsMappedToCounts[word]}\n")
